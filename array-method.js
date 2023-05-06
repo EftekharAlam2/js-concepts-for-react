c0ba62008e2a9e927b2a0263986a40d12a0d8439
EftekharAlam2 <hinsouvlmare@gmail.com>
2023-05-06 23:54:43 +0600

    Array Method added

---
+++ array-method.js
@@ -0,0 +1,23 @@
+const products = [
+  { name: "laptop", price: 3200, brand: "lenovo", color: "silver" },
+  { name: "phone", price: 7000, brand: "iphone", color: "golden" },
+  { name: "watch", price: 3000, brand: "casio", color: "yellow" },
+  { name: "sungalss", price: 300, brand: "ray", color: "black" },
+  { name: "camera", price: 9000, brand: "canon", color: "gray" },
+];
+
+const brands = products.map((product) => product.brand);
+console.log(brands);
+const prices = products.map((product) => product.price);
+console.log(prices);
+
+products.forEach((product) => {});
+
+const cheap = products.filter((product) => product.price <= 5000);
+console.log(cheap);
+
+const specificName = products.filter((p) => p.name.includes("n"));
+console.log(specificName);
+
+const special = products.find((p) => p.name.includes("n"));
+console.log(special);
